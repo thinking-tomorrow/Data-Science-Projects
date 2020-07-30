4e53b3061b77b263c59d983c5477766abbe9dcd0
Hrishikesh <hrishipotter123@gmail.com>
2020-07-30 22:27:34 +0530

    Added some basics of streamlit

--- hello.py
+++ hello.py
@@ -1,9 +1,25 @@
 import streamlit as st
 import pandas as pd
-
+import numpy as np
 
 st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
+
+df = pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
-}))
+})
+
+df
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+if st.checkbox('Show dataframe'):
+    map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+    st.map(map_data)
